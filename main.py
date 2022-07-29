80acf8c567bb593c4b8fa309041e2b22ec460f95
Syahrul Hidayat <syah.hidayat71@gmail.com>
2022-07-29 21:45:20 +0700

    Bot added to calculate next move

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+import sys
+import time
 from itertools import chain
 
 import cv2
 
 from model.board import Board
 import numpy as np
-import util.screen as sc
 
+import util.screen as sc
 
 # GAME SETTING
 BOARD_SIZE_W, BOARD_SIZE_H = BOARD_SIZE = (8, 8)
@@ -16,46 +18,141 @@ BLOCK_SIZE_W, BLOCK_SIZE_H = BLOCK_SIZE = (48, 48)
 PADDING_X, PADDING_Y = PADDING = (24, 24)
 WINDOW_SIZE_W, WINDOW_SIZE_H = WINDOW_SIZE = (PADDING_X * 2 + BOARD_SIZE_W * BLOCK_SIZE_W, PADDING_Y * 2 + BOARD_SIZE_H * BLOCK_SIZE_H)
 
+USER, COMPUTER = +1, -1
+
 board = Board(WINDOW_SIZE, PADDING, BOARD_SIZE, BLOCK_SIZE)
-player = 1
+player = USER
+
+is_game_over = False
+# winner = None
 
 
 def mouse_callback(event, px, py, flags, param):
-    global player
+    global player, is_game_over
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        # Convert ot board coordinate
+        # Convert to board coordinate
         x, y = (px - PADDING_X) // BLOCK_SIZE_W, (py - PADDING_Y) // BLOCK_SIZE_H
-        print(f'Mouse clicked at: {(px, py)}')
-        print(f'Board coordinate: {(x, y)}')
+        # print(f'Mouse clicked at: {(px, py)}')
+        # print(f'Board coordinate: {(x, y)}')
 
-        possible_moves = board.get_possible_moves(player)
-        possible_moves
-        if True in list(chain.from_iterable(possible_moves[y][x])):
-            print('Move: ', possible_moves[y][x])
-            board.put_player(player, (x, y), possible_moves[y][x])
-            player = -player
+        # User turn
+        move_possibilities, move_scores = board.get_possible_moves(player)
+        is_move_able = board.can_move(player)
+        if not is_move_able:
+            is_game_over = True
             show_window()
 
+        else:
+            if player == USER:
+                move_possibilities_list = list(chain.from_iterable(move_possibilities[y][x]))
+                if True in move_possibilities_list:
+                    # print('Move: ', move_possibilities[y][x])
+                    # print('Count: ', move_possibilities_list.count(True))
+                    board.put_player(player, (x, y), move_possibilities[y][x])
+                    player = -player
+                    show_window()
 
-def show_window():
+                # Computer turn
+                (x, y), score = board.next_move(player)
+                move_possibilities, move_scores = board.get_possible_moves(player)
+                # print(f'Computer choice: {(x, y)}')
+
+                if None in [x, y]:
+                    is_game_over = True
+                    show_window()
+
+                else:
+                    board.put_player(player, (x, y), move_possibilities[y][x])
+                    player = -player
+                    show_window()
+
+                    # User turn
+                    move_possibilities, move_scores = board.get_possible_moves(player)
+                    is_move_able = board.can_move(player)
+                    if not is_move_able:
+                        is_game_over = True
+                        show_window()
+
+
+def show_window(wait=True):
+    global is_game_over
+
     img = board.draw_board(player)
     cv2.imshow('Reversi', img)
+    if wait:
+        cv2.waitKey(1)
+        time.sleep(1)
+
+    sc.clear_screen()
+    value_user = board.value[board.value == USER]
+    score_user = abs(np.sum(value_user))
+
+    value_computer = board.value[board.value == COMPUTER]
+    score_computer = abs(np.sum(value_computer))
+
+    print('Reversi Game')
+    print(f'Score User: {score_user:03d}')
+    print(f'Score Computer: {score_computer:03d}')
+
+    if is_game_over:
+        sc.clear_screen()
+        print('GAME OVER')
+
+        # Calculate the score
+        winner = None
+        if score_user > score_computer:
+            print('YOU WIN')
+
+        elif score_user < score_computer:
+            print('YOU LOSE')
+
+        else:
+            print('DRAW')
+
+    print('Press "Q" to quit')
+    print('Press "R" to rematch or restart')
+
+    # else:
+        # key = cv2.waitKey(0)
+        # if key == ord('q') and key == ord('Q'):
+        #     print('Exit the game')
+        #     sys.exit(0)
+        #
+        # if key == ord('r') or key == ord('R'):
+        #     is_game_over = False
+        #     winner = False
+        #
+        #     board.rematch()
+        #
+        # show_window()
 
 
 def main():
-    sc.clear_screen()
+    global is_game_over, player
+
 
     WINDOW_TITLE = 'Reversi'
     cv2.namedWindow(WINDOW_TITLE)
     cv2.setMouseCallback(WINDOW_TITLE, mouse_callback)
+    show_window()
+
     while True:
-        show_window()
         key = cv2.waitKey(0)
-        if key == ord('q'):
+        print('Key pressed')
+        if key == ord('q') or key == ord('Q'):
+            print('Exit the game')
+            is_game_over = True
             break
 
-    print('Game Over')
+        if key == ord('r') or key == ord('R'):
+            is_game_over = False
+            player = USER
+
+            board.rematch()
+            show_window(wait=False)
+
+    sys.exit(0)
 
 
 if __name__ == '__main__':
--- model/board.py
+++ model/board.py
@@ -1,6 +1,7 @@
 from itertools import chain
 
 import cv2
+import random
 import numpy as np
 
 
@@ -21,6 +22,8 @@ class Board:
         h, w = value.shape
         x, y = (w // 2) - 1, (h // 2) - 1
 
+        # value[y - 2][x + 0], value[y - 2][x + 1] = +0, -1
+        # value[y - 1][x + 0], value[y - 1][x + 1] = +0, -1
         value[y + 0][x + 0], value[y + 0][x + 1] = +1, -1
         value[y + 1][x + 0], value[y + 1][x + 1] = -1, +1
 
@@ -56,11 +59,11 @@ class Board:
         return img
 
     def draw_possible_moves(self, img, player):
-        possible_moves = self.get_possible_moves(player)
-        h, w = possible_moves.shape[:2]
+        move_possibilities, move_scores = self.get_possible_moves(player)
+        h, w = move_possibilities.shape[:2]
         for x in range(w):
             for y in range(h):
-                val = possible_moves[y][x]
+                val = move_possibilities[y][x]
                 if True in list(chain.from_iterable(val)):
                     xc = self.padding_w + x * self.block_w + self.block_w // 2
                     yc = self.padding_h + y * self.block_h + self.block_h // 2
@@ -91,27 +94,29 @@ class Board:
         return img
 
     def get_possible_moves(self, player):
-        moves = np.zeros((self.board_h, self.board_w, 3, 3), dtype=bool)
+        move_possibilities = np.zeros((self.board_h, self.board_w, 3, 3), dtype=bool)
+        move_scores = np.zeros((self.board_h, self.board_w), dtype=int)
 
         for y in range(self.board_h):
             for x in range(self.board_w):
                 val = self.value[y][x]
                 if val == player:
-                    # Vertical
-                    moves = self.get_diagonal_possibility(x, y, player, moves, directions=[(0, -1), (0, 1)])
+                    # # Vertical
+                    move_possibilities, move_scores = self.get_diagonal_possibility(x, y, player, move_possibilities, move_scores, directions=[(0, -1), (0, 1)])
 
                     # Horizontal
-                    moves = self.get_diagonal_possibility(x, y, player, moves, directions=[(-1, 0), (1, 0)])
+                    move_possibilities, move_scores = self.get_diagonal_possibility(x, y, player, move_possibilities, move_scores, directions=[(-1, 0), (1, 0)])
+                    # move_possibilities, move_scores = self.get_diagonal_possibility(x, y, player, move_possibilities, move_scores, directions=[(1, 0)])
 
                     # Diagonal
-                    moves = self.get_diagonal_possibility(x, y, player, moves)
+                    move_possibilities, move_scores = self.get_diagonal_possibility(x, y, player, move_possibilities, move_scores)
 
-        return moves
+        return move_possibilities, move_scores
 
     def put_player(self, player, coordinate, mode):
         x, y = coordinate
         self.value[y][x] = player
-        print(f'coordinate: {coordinate}')
+        # print(f'coordinate: {coordinate}')
 
         # Vertical move
         for direction, (start, end, inc) in [(mode[0][1], [y + 1, self.board_h, 1]), (mode[2][1], [y - 1, -1, -1])]:
@@ -125,7 +130,7 @@ class Board:
 
         # Horizontal move
         for direction, (start, end, inc) in [(mode[1][0], [x + 1, self.board_w, +1]), (mode[1][2], [x - 1, -1, -1])]:
-            print(f'(direction, (start, end, inc)): {direction, (start, end, inc)}')
+            # print(f'(direction, (start, end, inc)): {direction, (start, end, inc)}')
             if not direction:
                 continue
             for x1 in range(start, end, inc):
@@ -138,7 +143,7 @@ class Board:
         for direction, (dx, dy) in [(mode[0][0], (+1, +1)), (mode[0][2], (-1, +1)), (mode[2][0], (1, -1)), (mode[2][2], (-1, -1))]:
             if not direction:
                 continue
-            print(f'DIAGONAL: {(dx, dy)}')
+            # print(f'DIAGONAL: {(dx, dy)}')
             i = 1
             while True:
                 x1, y1 = x + dx * (i + 0), y + dy * (i + 0)
@@ -153,7 +158,7 @@ class Board:
 
                 i += 1
 
-    def get_vertical_possibility(self, x, y, player, moves, mode='up'):
+    def get_vertical_possibility(self, x, y, player, move_possibilities, mode='up'):
         if mode == 'up':
             c, d = 1, 0
             y_start, y_end, y_inc = (y - 1, -1, -1)
@@ -175,11 +180,11 @@ class Board:
 
         if coordinate != (None, None):
             a, b = coordinate
-            moves[b][a][d][c] = True
+            move_possibilities[b][a][d][c] = True
 
-        return moves
+        return move_possibilities
 
-    def get_horizontal_possibility(self, x, y, player, moves, mode='left'):
+    def get_horizontal_possibility(self, x, y, player, move_possibilities, mode='left'):
         if mode == 'left':
             c, d = 0, 1
             x_start, x_end, x_inc = (x - 1, -1, -1)
@@ -201,28 +206,29 @@ class Board:
 
         if coordinate != (None, None):
             a, b = coordinate
-            moves[b][a][d][c] = True
+            move_possibilities[b][a][d][c] = True
 
-        return moves
+        return move_possibilities
 
-    def get_diagonal_possibility(self, x, y, player, moves, directions=None):
+    def get_diagonal_possibility(self, x, y, player, move_possibilities, move_scores, directions=None):
         if directions is None:
             directions = [(-1, -1), (1, -1), (-1, 1), (1, 1)]
 
-        print(f'player: {player}')
-        print(f'(x, y): {(x, y)}')
-        print(f'directions: {directions}')
+        # print(f'player: {player}')
+        # print(f'(x, y): {(x, y)}')
+        # print(f'directions: {directions}')
         for (dx, dy) in directions:
             c, d = 1 + dx, 1 + dy
-            print(f'(dx, dy): {(dx, dy)}')
+            # print(f'(dx, dy): {(dx, dy)}')
             i = 2
+            score = 0
             coordinate = (None, None)
             while True:
                 x1, y1 = x + dx * (i + 0), y + dy * (i + 0)
                 x2, y2 = x + dx * (i - 1), y + dy * (i - 1)
 
-                print(f'\t(x1, y1): {(x1, y1)}')
-                print(f'\t(x2, y2): {(x2, y2)}')
+                # print(f'\t(x1, y1): {(x1, y1)}')
+                # print(f'\t(x2, y2): {(x2, y2)}')
 
                 if ((x1 < 0 or self.board_w <= x1)
                         or (x2 < 0 or self.board_w <= x2)
@@ -234,11 +240,11 @@ class Board:
 
                 if (0 <= x1 < self.board_w) and (0 <= y1 < self.board_h):
                     val1 = self.value[y1][x1]
-                print(f'\tval1: {val1}')
+                # print(f'\tval1: {val1}')
 
                 if (0 <= x2 < self.board_w) and (0 <= y2 < self.board_h):
                     val2 = self.value[y2][x2]
-                print(f'\tval2: {val2}')
+                # print(f'\tval2: {val2}')
 
                 if val1 is None:
                     val1 = 0
@@ -249,23 +255,79 @@ class Board:
                 if (val1 is None) and (val2 is None):
                     break
 
-                if val1 == 0:
-                    if val2 == -player:
-                        coordinate = (x1, y1)
-                        print(f'\t\tcoordinate: {coordinate}')
-                    elif val2 == player:
-                        coordinate = (None, None)
+                if val1 == 0 and val2 == -player:
+                    coordinate = (x1, y1)
+                    score += 1
+                    # print(f'\t\tcoordinate: {coordinate}')
+                    break
+
+                elif val1 == -player and val2 == -player:
+                    i += 1
+                    score += 1
+
                 else:
-                    if coordinate != (None, None):
-                        coordinate = (None, None)
-                        break
+                    break
 
-                i += 1
-
-            print(f'  RESULT: {coordinate}')
+            # print(f'  RESULT: {coordinate}')
             a, b = coordinate
             if a is not None and b is not None:
-                moves[b][a][d][c] = True
-                print(f'  UPDATE: {coordinate}')
+                move_possibilities[b][a][d][c] = True
+                move_scores[b][a] = score
+                # print(f'  UPDATE: {coordinate}')
 
-        return moves
+        return move_possibilities, move_scores
+
+    def next_move(self, player):
+
+        move_possibilities, move_scores = self.get_possible_moves(player)
+
+        count = 1
+        possibilities = []
+        scores = []
+        for y_ in range(self.board_h):
+            for x_ in range(self.board_w):
+                move_possibilities_list = list(chain.from_iterable(move_possibilities[y_][x_]))
+                count_ = move_possibilities_list.count(True)
+                # print(f'count_ at ({(x_, y_)}): {count_}')
+
+                if count_ > count:
+                    count = count_
+                    possibilities = [(x_, y_)]
+                    scores = [move_scores[y_, x_]]
+
+                elif count == count_:
+                    possibilities.append((x_, y_))
+                    scores.append(move_scores[y_, x_])
+
+        if len(scores) > 0:
+            score_max = np.max(scores)
+            # print(f'score_max: {score_max}')
+
+            result_possibilities, result_score = [], []
+            for i in range(len(possibilities)):
+                possibility, score = possibilities[i], scores[i]
+                # print(f'score: {score}')
+
+                if score == score_max:
+                    result_possibilities.append(possibility)
+                    result_score.append(score)
+
+            if len(result_possibilities) > 0:
+                idx = random.randint(0, len(result_possibilities) - 1)
+                return result_possibilities[idx], result_score[idx]
+
+        else:
+            return (None, None), None
+
+    def can_move(self, player):
+        move_possibilities, move_scores = self.get_possible_moves(player)
+        for x in range(self.board_w):
+            for y in range(self.board_h):
+                move_possibilities_list = list(chain.from_iterable(move_possibilities[y][x]))
+                count = move_possibilities_list.count(True)
+                if count > 0:
+                    return True
+        return False
+
+    def rematch(self):
+        self.value = self.create_board()
